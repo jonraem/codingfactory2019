1738564538301cba2caae4e092c1910c967d33ab
Joni Rämö <joni.ramo.91@gmail.com>
2019-02-06 19:47:28 +0200

    Add mirror test

---
+++ src/gateTwo.js
@@ -0,0 +1,3 @@
+export function mirror() {
+  // Write code here!
+}
--- tests/gateTwo.test.js
+++ tests/gateTwo.test.js
@@ -1,8 +1,8 @@
 import gateTwo from '../src/gateTwo';
 
 describe('Gate Two', () => {
-  test('hard test 1', () => {
-    expect(true).toEqual(true);
+  test('mirrors text', () => {
+    expect(mirror('Hello World!')).toEqual('!dlroW olleH');
   });
 
   test('hard test 2', () => {
