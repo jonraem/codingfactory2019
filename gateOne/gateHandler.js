df9d7fa99906796a7045329c15fb71f0b28c9feb
Niko Salminen <niko@salminen.me>
2019-02-08 13:35:54 +0200

    Correct gate addresses

--- gateOne/gateHandler.js
+++ gateOne/gateHandler.js
@@ -1,7 +1,7 @@
 import needle from 'needle';
 
 export default () => {
-  const url = 'http://192.268.x.x/gate1/open'; // REPLACE THIS WITH ACTUAL URL
+  const url = 'http://192.168.10.61/gate1/open'; // REPLACE THIS WITH ACTUAL URL
   needle.get(url, (err, res) => {
     if (err) {
       console.log(err);
